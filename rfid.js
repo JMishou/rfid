c87a7e0d2d160ffac97eddaee013f93408cb3b91
MISH <jason.mishou@gmail.com>
2016-09-15 10:54:28 -0500

    temp add access ids

--- rfid.js
+++ rfid.js
@@ -1,7 +1,11 @@
+var RFIDData;
 var serdata = [];
 var SerialPort = require("serialport");
 var serialport = new SerialPort("/dev/ttyAMA0", 9600);
 var lastRFIDTime = 0;
+
+
+loadRFID();
 serialport.on('open', function() {
     console.log('Serial Port Opened');
     serialport.on('data', function(data) {
@@ -21,6 +25,7 @@ serialport.on('open', function() {
             testrfid = rfidstring.join("");
             testrfid = pad(parseInt("0x" + testrfid.slice(4,10)),10));  
             console.log(testrfid);
+	    console.log(lookupRFID(testrfid));
         }
     });
 });
@@ -28,4 +33,28 @@ serialport.on('open', function() {
 function pad(num, size) {
     var s = "000000000" + num;
     return s.substr(s.length-size);
+}
+
+function loadRFID() {
+    fs.readFile('accessRFID.json', function(err, data) {
+        if (err) {
+            console.log('No RFID File Found contacting server…');
+            RFIDLoadInterval = setInterval(loadRFIDServer, 30000);
+            return;
+        }
+        RFIDData = JSON.parse(data);
+        console.log("RFID Data Loaded from File.");
+        dataState = 1;
+    });
+}
+
+function lookupRFID(testrfid) {
+    //var num = "0008369167"
+    var keys = [];
+    for (var key in RFIDData["rfid"]) {
+        if (RFIDData["rfid"].hasOwnProperty(key)) {
+            if (RFIDData["rfid"][key].indexOf(testrfid) != -1) return key;
+        }
+    }
+    return -1;
 }
